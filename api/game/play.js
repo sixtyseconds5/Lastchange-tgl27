5293ec40ac5bfdf959dece9f344e4a0a5e6f8f06
Mulyana <116785923+sixtyseconds5@users.noreply.github.com>
2025-09-27 03:54:08 +0700

    Add files via upload

---
+++ api/game/play.js
@@ -0,0 +1,8 @@
+// api/game/play.js
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    res.status(200).json({ ok: true, message: 'Game started' })
+  } else {
+    res.status(405).json({ error: 'Method not allowed' })
+  }
+}
